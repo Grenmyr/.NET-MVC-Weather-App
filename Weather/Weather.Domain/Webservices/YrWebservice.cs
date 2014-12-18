37d28b10a1c3298f9553450876882631be08ae1d
Grenmyr <dg222cs@student.lnu.se>
2014-12-18 14:21:25 +0100

    #linq uttryck färdig för göra prognos nu och 4 dygn famöver

--- Weather/Weather.Domain/Webservices/YrWebservice.cs
+++ Weather/Weather.Domain/Webservices/YrWebservice.cs
@@ -20,7 +20,7 @@ namespace Weather.Domain.Webservices
         {
             XDocument xmlResponse;
             String searchObject;
-            var urlString = String.Format("http://api.yr.no/weatherapi/locationforecast/1.9/?lat={0};lon={1}", location.Lng, location.Lat);
+            var urlString = String.Format("http://api.yr.no/weatherapi/locationforecast/1.9/?lat={0};lon={1}", location.Lat, location.Lng);
 
             var webRequest = WebRequest.Create(urlString);
 
@@ -32,14 +32,15 @@ namespace Weather.Domain.Webservices
                 //searchObject = xmlResponse.Root;
 
             }
-
+            var alltimes = xmlResponse.Descendants("time");
+            var current = alltimes.Take(2).ToList();
+            var forcasts = alltimes.Skip(2).Where(d => d.Attribute("to").Value.Contains("12:00")
+                                            && d.Attribute("from").Value.Contains("12:00"))
+                                  .SelectMany(n => new[] {n, n.NextNode}).Take(8).ToList();
             //var listOne = xmlResponse.Descendants("time").Select(d => d).Where(d => d.LastAttribute.ToString().Contains("15:00")).ToList();
-            var listTwo = xmlResponse.Descendants("time")
-                .Select(d => d)
-                .Where(d => d.Attribute("to").Value
-                .Contains("15:00")).ToList();
-
-            var listThree = xmlResponse.Descendants("time").Select(d => d).ToList();
+            
+            //var listfour = 
+            //var listThree = xmlResponse.Descendants("time").Select(d => d).ToList();
             //var list = xmlResponse.Descendants("time").First();
 
             //var items = (from forecast in xmlResponse.Descendants("time")
